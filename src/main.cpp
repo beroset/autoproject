1b9510bbb916c40d650f0cf1f361b0f55232b4c0
Ed Beroset <beroset@ieee.org>
2020-06-22 20:50:06 -0400

    Fix #7 by implementing --configfile command line argument.

--- src/main.cpp
+++ src/main.cpp
@@ -34,10 +34,10 @@ constexpr std::string_view license{R"(
 
 )"};
 
-static const std::string configfilename{DATAFILE_DIR "/config/autoproject.conf"};
+static const std::string defaultconfigfilename{DATAFILE_DIR "/config/autoproject.conf"};
 
 int main(int argc, char *argv[]) {
-    ConfigFile cfg{configfilename};
+    std::string configfile{defaultconfigfilename};
     struct {
         std::string configfiledir;
         std::string rulesfilename;
@@ -45,10 +45,76 @@ int main(int argc, char *argv[]) {
         std::string srclevelcmakefilename;
         bool forceOverwrite = false;
         bool license = false;
+        bool help = false;
     } configuration;
+
+    // handle command line arguments
+    std::map<std::string, bool&> boolargs{
+        { "--forceoverwrite", configuration.forceOverwrite },
+        { "--license", configuration.license },
+        { "--help", configuration.help },
+    };
+    // TODO: use this to allow override of configuration file
+    std::map<std::string, std::string&> stringargs{
+        { "--configfile", configfile},
+    };
+    std::map<std::string, std::string> shortargs{
+        { "-c", "--configfile" },
+        { "-f", "--forceoverwrite" },
+        { "-L", "--license" },
+    };
+    // TODO: make a more rational system for command line args
+    // Specifically, command line args should override config file.
+    // What's the best way to do that?
+    int processed_args{0};
+    for (int i=1; i < argc; ++i) {
+        auto option = boolargs.find(argv[i]);
+        if (option != boolargs.end()) {
+            std::cout << "Found option " << option->first << '\n';
+            option->second = true;
+            ++processed_args;
+        }
+
+        auto stroption = stringargs.find(argv[i]);
+        if (stroption != stringargs.end()) {
+            std::cout << "Found option " << stroption->first << '\n';
+            stroption->second = argv[i+1];
+            ++processed_args;
+        }
+
+        auto shortoption = shortargs.find(argv[i]);
+        if (shortoption != shortargs.end()) {
+            std::cout << "Found option " << shortoption->first << '\n';
+            option = boolargs.find(shortoption->second);
+            option->second = true;
+            ++processed_args;
+        }
+
+        auto shortstroption = shortargs.find(argv[i]);
+        if (shortstroption != shortargs.end()) {
+            std::cout << "Found option " << shortoption->first << '\n';
+            stroption = stringargs.find(shortstroption->second);
+            stroption->second = argv[i+1];
+            ++processed_args;
+        }
+    }
+    if (configuration.license) {
+        std::cout << license;
+    }
+    if (configuration.help) {
+        std::cout << "Usage ....\n";
+        return 0;
+    }
+    std::ifstream config{configfile};
+    if (!config) {
+        std::cerr << "Error: cannot open input configuration file \"" << configfile << "\"\n";
+        return 1;
+    }
+    ConfigFile cfg{config};
+
     if (std::stoi(cfg.get_value("General", "Version")) != VERSION_MAJOR) {
-        std::cerr << "Error: version in " << configfilename << " does not equal " << VERSION_MAJOR << "\n";
-    } else {
+        std::cerr << "Error: version in " << configfile << " does not equal " << VERSION_MAJOR << "\n";
+    } else if (!configuration.forceOverwrite) {
         auto over{cfg.get_value("General", "ForceOverwrite")};
         if (over == "true" || over == "TRUE" || over == "True") {
             configuration.forceOverwrite = true;
@@ -59,33 +125,6 @@ int main(int argc, char *argv[]) {
     configuration.toplevelcmakefilename = configuration.configfiledir + "/" + cfg.get_value("General", "TopLevelCMakeFileName");
     configuration.srclevelcmakefilename = configuration.configfiledir + "/" + cfg.get_value("General", "SrcLevelCMakeFileName");
 
-    std::map<std::string, bool&> args{
-        { "--force-overwrite", configuration.forceOverwrite },
-        { "--license", configuration.license },
-    };
-    std::map<std::string, std::string> shortargs{
-        { "-f", "--force-overwrite" },
-        { "-L", "--license" },
-    };
-    int processed_args{0};
-    for (int i=1; i < argc; ++i) {
-        auto option = args.find(argv[i]);
-        if (option != args.end()) {
-            std::cout << "Found option " << option->first << '\n';
-            option->second = true;
-            ++processed_args;
-        }
-        auto shortoption = shortargs.find(argv[i]);
-        if (shortoption != shortargs.end()) {
-            std::cout << "Found option " << shortoption->first << '\n';
-            option = args.find(shortoption->second);
-            option->second = true;
-            ++processed_args;
-        }
-    }
-    if (configuration.license) {
-        std::cout << license;
-    }
     if (argc - processed_args != 2) {
         std::cerr << "Usage: autoproject project.md\nCreates a CMake build tree under 'project' subdirectory\n";
         return 0;
